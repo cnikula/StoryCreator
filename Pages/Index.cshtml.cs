45a1cd6867318fb10da97234b57e051bbca9a293
Claude R Nikula <cnikula@mesquiteit.com>
2024-03-14 15:33:36 -0500

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using Azure.AI.OpenAI;
+using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace StoryCreator.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string StoryContent { get; set; }
+        public string Title { get; set; }
+
+        [BindProperty]
+        public StoryDetails Details { get; set; } = new StoryDetails();
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnPost()
+        {
+            string endpoint = "https://mitai.openai.azure.com/";
+            string key = "e67e1ba43c3244d5bd618fbc2a8f282c";
+            string deploymentName = "turb035mitai";
+
+            OpenAIClient client = new(new Uri(endpoint), new AzureKeyCredential(key));
+
+            string prompt = $"Write me a story called {Details.Title} in a {Details.Tone} tone about a " +
+                $"{Details.Animal} named {Details.Name} who lives in a {Details.Environment}.";
+
+            ChatCompletionsOptions chatCompletionsOptions = new ChatCompletionsOptions()
+            {
+                Messages =
+                {
+                    new ChatMessage(ChatRole.System, "You are a helpful AI Bot."),
+                    new ChatMessage(ChatRole.User, prompt)
+                }
+            };
+
+            // Send request to Azure OpenAI model
+            ChatCompletions chatCompletionsResponse = client.GetChatCompletions(deploymentName, chatCompletionsOptions);
+
+            StoryContent = chatCompletionsResponse.Choices[0].Message.Content;
+        }
+    }
+
+    public class StoryDetails
+    {
+        public string Title { get; set; }
+        public string Tone { get; set; }
+        public string Name { get; set; }
+        public string Animal { get; set; }
+        public string Environment { get; set; }
+
+    }
+}
